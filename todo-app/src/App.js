63844090e5489c62e8a638373e784f5f672fef4c
kassandra-vasquez <kyanelyyy@gmail.com>
2024-07-13 16:43:23 -0700

    todoform

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import './App.css';
+import TodoForm from "./components/TodoForm";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Todo App</h1>
+    <div className="todo-app">
+      <TodoForm />
     </div>
   );
 }
